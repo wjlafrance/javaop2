88245f907b3482b2def15ff47d1b0ef4a91afa67
William LaFrance <admin@wjlafrance.net>
2011-08-19 17:33:37 -0500

    Worked on tc_CheckRevision

--- spec/tc_CheckRevision.rb
+++ spec/tc_CheckRevision.rb
@@ -16,106 +16,94 @@
 include Java 
 require "test/unit"
 
-def setup
-end
+class TestCheckRevision < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown 
+    File.delete("_GameData.txt")
+    File.delete("_GlobalSettings.txt")
+  end
 
 
-def teardown 
-  File.delete("_GameData.txt")
-  File.delete("_GlobalSettings.txt")
-end
-
-
-
-def testCheckRevisionClassExists
+  def testCheckRevisionClassExists
         Assert.assertNotNull(CheckRevision.class);
-end
-
-def testCheckRevisionMethodExists
-        Class[] CheckRevision_doCheckRevisionReturnType = new Class[] {
-                String.class,
-                String[].class,
-                byte[].class
-        };
-        Method localMethod = CheckRevision.class.getMethod("doCheckRevision", CheckRevision_doCheckRevisionReturnType);
-        Assert.assertNotNull(localMethod);
-end
+  end
 
 
-def testCheckRevisionMethodCorrectReturnType
-        Class[] CheckRevision_doCheckRevisionReturnType = new Class[] {
-                String.class,
-                String[].class,
-                byte[].class
-        };
-        Method localMethod = CheckRevision.class.getMethod("doCheckRevision", CheckRevision_doCheckRevisionReturnType);
-        Assert.assertEquals(int.class, localMethod.getReturnType());
-end
+  def testCheckRevisionMethodExists
+        #Class[] CheckRevision_doCheckRevisionReturnType = new Class[] {
+        #        String.class,
+        #        String[].class,
+        #        byte[].class
+        #};
+        #Method localMethod = CheckRevision.class.getMethod("doCheckRevision", CheckRevision_doCheckRevisionReturnType);
+        #Assert.assertNotNull(localMethod);
+  end
 
 
-def testCheckRevisionAgainstBnls
-        CheckRevisionResults d2dvBnlsResults = Bnls.CheckRevision(
-                new Game("D2DV"),
-                "ver-IX86-6.mpq",
-                (long)0x1c75f7003518b00l,
-                "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
-        int d2dvLocalHashChecksum = CheckRevision.doCheckRevision("ver-IX86-6.mpq",
-                new String[] {
-                        System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
-                        System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
-                        System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
-                },
-                "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
-        Assert.assertEquals(d2dvBnlsResults.checksum, d2dvLocalHashChecksum);
-end
+  def testCheckRevisionMethodCorrectReturnType
+        #Class[] CheckRevision_doCheckRevisionReturnType = new Class[] {
+        #        String.class,
+        #        String[].class,
+        #        byte[].class
+        #};
+        #Method localMethod = CheckRevision.class.getMethod("doCheckRevision", CheckRevision_doCheckRevisionReturnType);
+        #Assert.assertEquals(int.class, localMethod.getReturnType());
+  end
 
 
-def testDiablo2LocalOldCheckRevisionAgainstBnls
-        CheckRevisionResults d2dvBnlsResults = Bnls.CheckRevision(
-                new Game("D2DV"),
-                "ver-IX86-6.mpq",
-                (long)0x1c75f7003518b00l,
-                "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
-        int d2dvLocalHashChecksum = CheckRevision.checkRevisionOld("IX86",  
-                6,
-                new String[] {
-                    System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
-                    System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
-                    System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
-                },
-                "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B");
-        Assert.assertEquals(d2dvBnlsResults.checksum, d2dvLocalHashChecksum);
-end
+  def testCheckRevisionAgainstBnls
+        #CheckRevisionResults d2dvBnlsResults = Bnls.CheckRevision(
+        #        new Game("D2DV"),
+        #        "ver-IX86-6.mpq",
+        #        (long)0x1c75f7003518b00l,
+        #        "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
+        #int d2dvLocalHashChecksum = CheckRevision.doCheckRevision("ver-IX86-6.mpq",
+        #        new String[] {
+        #                System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
+        #                System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
+        #                System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
+        #        },
+        #        "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
+        #Assert.assertEquals(d2dvBnlsResults.checksum, d2dvLocalHashChecksum);
+  end
 
 
-def testDiablo2LocalOldSlowCheckRevisionAgainstBnls
-        /*
-        Method checkRevisionOldSlow = CheckRevision.class.getMethod("checkRevisionOldSlow", checkRevisionOldSlowArguments);     
-        checkRevisionOldSlow.setAccessible(true);
-        int d2dvLocalHashChecksum = (Integer)checkRevisionOldSlow.invoke(null,
-                new Object[] {
-                    "IX86",
-                    6,
-                    new String[] {
-                        System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
-                        System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
-                        System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
-                    },
-                    "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B"
-                } );
-        */
-        CheckRevisionResults d2dvBnlsResults = Bnls.CheckRevision(
-                new Game("D2DV"),
-                "ver-IX86-6.mpq",
-                (long)0x1c75f7003518b00l,
-                "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
-        int d2dvLocalHashChecksum = CheckRevision.checkRevisionOldSlow("IX86",  
-                6,
-                new String[] {
-                    System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
-                    System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
-                    System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
-                },
-                "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B");
-        Assert.assertEquals(d2dvBnlsResults.checksum, d2dvLocalHashChecksum);
+  def testDiablo2LocalOldCheckRevisionAgainstBnls
+        #CheckRevisionResults d2dvBnlsResults = Bnls.CheckRevision(
+        #        new Game("D2DV"),
+        #        "ver-IX86-6.mpq",
+        #        (long)0x1c75f7003518b00l,
+        #        "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
+        #int d2dvLocalHashChecksum = CheckRevision.checkRevisionOld("IX86",  
+        #        6,
+        #        new String[] {
+        #            System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
+        #            System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
+        #            System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
+        #        },
+        #        "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B");
+        #Assert.assertEquals(d2dvBnlsResults.checksum, d2dvLocalHashChecksum);
+  end
+
+
+  def testDiablo2LocalOldSlowCheckRevisionAgainstBnls
+        #CheckRevisionResults d2dvBnlsResults = Bnls.CheckRevision(
+        #        new Game("D2DV"),
+        #        "ver-IX86-6.mpq",
+        #        (long)0x1c75f7003518b00l,
+        #        "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B".getBytes());
+        #int d2dvLocalHashChecksum = CheckRevision.checkRevisionOldSlow("IX86",  
+        #        6,
+        #        new String[] {
+        #            System.getProperty("user.dir") + "/Hashfiles/D2DV/Game.exe",
+        #            System.getProperty("user.dir") + "/Hashfiles/D2DV/Bnclient.dll",
+        #            System.getProperty("user.dir") + "/Hashfiles/D2DV/D2Client.dll"
+        #        },
+        #        "A=3116746618 B=3629198554 C=229279865 4 A=A+S B=B-C C=C^A A=A^B");
+        #Assert.assertEquals(d2dvBnlsResults.checksum, d2dvLocalHashChecksum);
+  end
+
 end
